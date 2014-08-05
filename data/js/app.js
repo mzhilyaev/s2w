8a339671a4e4bd0b68de6c787755896150c1e3e4
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-08-05 13:54:01 +0400

    Fixes #7: dashboard shows pledged sites only. add a link for nominated

--- data/js/app.js
+++ data/js/app.js
@@ -112,34 +112,50 @@ pledgedSitesApp.controller("dashboardCtrl", function($scope) {
     self.port.emit("command", {token: token, site: site});
   }
 
+  $scope.toggleNominatedList = function() {
+    $scope.showNominated = !($scope.showNominated);
+  }
+
   self.port.on("data", function(data) {
     let {usage, pledges} = data;
     let domains = usage.domains;
-    let total = usage.total;
+    let topSites = [];
     let sorted = [];
+    let visited = {};
+    let nominated = [];
 
+    // populate sorted with visited participating sites first
     Object.keys(domains).sort(function(a, b) {
       return domains[b] - domains[a];
     }).forEach(domain => {
-      sorted.push([domain, domains[domain], pledges.sites[domain]]);
+      // only collect sites that are pledged
+      if (pledges.sites[domain] && pledges.sites[domain].pledged) {
+        sorted.push([domain, domains[domain], pledges.sites[domain]]);
+        visited[domain] = true;
+        topSites.push([domain, domains[domain]]);
+      }
+    });
+
+    // add participating sites that were not visited
+    Object.keys(pledges.sites).forEach(domain => {
+      if (!visited[domain] && pledges.sites[domain].pledged) {
+        sorted.push([domain, 0, pledges.sites[domain]]);
+      }
+      // add nominated sites
+      else if (pledges.sites[domain].nominated) {
+        nominated.push([domain, domains[domain]]);
+      }
     });
 
     $scope.$apply(_ => {
       $scope.usage = usage;
-      $scope.sites = sorted.slice(0,50);
+      $scope.sites = sorted;
+      $scope.nominated = nominated;
     });
-
-    let topSites = [];
-    sorted.slice(0, 12).forEach(item => {
-      total -= item[1];
-      topSites.push([item[0], item[1]]);
-    });
-    // add ... for the rest of the sites
-    topSites.push(["...", total]);
-
+    /* @TODO - the graph needs to change to bars and refelct pledged % of visisted sites
     $("#visitChart").empty();
     // draw a pie chart
-    let catsPie = $.jqplot("visitChart", [topSites], {
+    let catsPie = $.jqplot("visitChart", [topSites.slice(0,12)], {
       grid: {
         background: "transparent",
         drawBorder: false,
@@ -163,6 +179,7 @@ pledgedSitesApp.controller("dashboardCtrl", function($scope) {
         shadow: false,
       },
     });
+    */
   });
 });
 
