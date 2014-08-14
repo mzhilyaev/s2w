899cbc970ff9652fd1b7b36ca2410276a950c3d1
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-08-14 13:56:05 +0400

    Fixes #20: Use disconnect and tpl domains for blocking

--- scripts/genBlockers.pl
+++ scripts/genBlockers.pl
@@ -3,25 +3,9 @@
 # to run 
 # git clone git@github.com:monicachew/abp-hostblocking.git
 # cd abp-hostblocking
-# run make
-# modify handler_abp.py to print offending urls to stdout
-#  diff --git a/handler_abp.py b/handler_abp.py
-#  index f5c454b..f301e80 100644
-#  --- a/handler_abp.py
-#  +++ b/handler_abp.py
-#  @@ -316,7 +316,7 @@ def find_hosts(filename, f_out, f_dbg, f_log, chunk):
-#           # book keeping
-#           domain_dict[match_s] = 1;
-#   
-#  -        #f_out.write("%s\n" % (match_s, ))
-#  +        sys.stdout.write("%s\n" % (match_s, ))
-#           output_dbg.append(hashlib.sha256(match_s).hexdigest());
-#           output.append(hashlib.sha256(match_s).digest());
-#  
-#
-# ./lists2safebrowsing.py tmp_in/abp tmp_out/abp/mozpub-track-digest256 > blockers.txt
-# now run genBlockers.js 
-# genBlockers.js blockers.txt > Blockers.js
+# git co disconnect
+# run make domainlist
+# genBlockers.js abp-hostblocking/tmp_out/domainlist/domains.txt> Blockers.js
 #
 
 use strict;
@@ -34,13 +18,7 @@ my $hosts = {};
 open(BLOCKERS, "< @ARGV[0]") || die("failed to open @ARGV[0]");
 while (<BLOCKERS>) {
   chomp($_);
-  $_ =~ s/\// \//;
-  $_ =~ s/[?].*$//;
-  my ($host, $path) = split(/ /,$_);
-  if (!$hosts->{$host}) {
-    $hosts->{$host} = [];
-  }
-  push @{$hosts->{$host}}, $path;
+  $hosts->{$_} = 1;
 }
 
 #print Dumper($hosts);
